f7180c337653addd39281ae020a1b482833a0b24
a1102827 <jmkandulu@gmail.com>
2024-11-27 14:20:33 +1030

    Fixed the here/base_path issue by creating a pure function 'createPath'

--- writeData.R
+++ writeData.R
@@ -3,7 +3,7 @@ writeData = function(data) {
     #check if directory exists, if not create
     base_path = getwd()
     data_folder_path = paste(base_path, "/data", sep="")
-    data_file_path = paste(data_folder_path, "data.csv", sep="")
+    data_file_path = paste(data_folder_path, "/data.csv", sep="")
 
     if(dir.exists(data_folder_path)){
         #if file exists then delete
